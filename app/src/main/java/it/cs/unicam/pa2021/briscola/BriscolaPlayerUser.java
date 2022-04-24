69dff81df016868b92fb262a407fa6ca2089de6b
Lorenzo Verducci <verduccii46@gmail.com>
2022-04-24 16:26:08 +0200

    Gestione Cast

---
+++ app/src/main/java/it/cs/unicam/pa2021/briscola/BriscolaPlayerUser.java
@@ -0,0 +1,8 @@
+package it.cs.unicam.pa2021.briscola;
+
+public class BriscolaPlayerUser extends BriscolaPlayer{
+
+    public BriscolaPlayerUser(String name, BriscolaHandUser hand) {
+        super(name, hand);
+    }
+}
--- app/src/main/java/it/cs/unicam/pa2021/briscola/BriscolaPlyerUser.java
+++
@@ -1,8 +0,0 @@
-package it.cs.unicam.pa2021.briscola;
-
-public class BriscolaPlyerUser extends BriscolaPlayer{
-
-    public BriscolaPlyerUser(String name, BriscolaHandUser hand) {
-        super(name, hand);
-    }
-}
